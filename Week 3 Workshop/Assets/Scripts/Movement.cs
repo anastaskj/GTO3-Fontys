f38e2d170aa412bb09b5950e060f8146a7da1e62
Kuyumdzhiev,Anastas A.A <a.kuyumdzhiev@student.fontys.nl>
2018-09-14 16:03:41 +0200

    week 3

---
+++ Week 3 Workshop/Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Vector2 movement = Vector2.zero;
+    public Vector2 check = Vector2.zero;
+    public PlayerInput input;
+    private Rigidbody2D character;
+    private Animator anim;
+    public Triggers enemy;
+    public float movementSpeed = 50;
+
+    void Start()
+    {
+        input = GetComponent<PlayerInput>();
+        character = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        enemy = GetComponent<Triggers>();
+    }
+
+    void FixedUpdate()
+    {
+        int tapDirection = this.input.Tap();
+        movement = new Vector2(tapDirection * 5, 0) * Time.deltaTime * this.movementSpeed;
+        check = new Vector2(-tapDirection, 0);
+        anim.SetFloat("Speed", tapDirection);
+        character.velocity = movement;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pillars"))
+        {
+            enemy.Chase();
+            Debug.Log("enabled");
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Week 3 Workshop/Assets/Scripts/Triggers.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggers : MonoBehaviour {
+
+    private Animator anim;
+    public Movement player;
+    private Rigidbody2D enemy;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        enemy = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            anim.SetBool("HasSpawned", true);
+            other.gameObject.SetActive(false);
+        }
+    }
+
+
+    public void Chase()
+    {
+        float checker = Vector2.Distance(player.movement, player.check);
+        Debug.Log("checking");
+        if (checker == 2)
+        {
+            Debug.Log("going");
+            enemy.velocity = -player.movement;
+        }
+       
+    }
+}
